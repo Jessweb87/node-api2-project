546409d989af74cee3e846c0cb700ee0782197bc
Jessweb87 <jprosper87@gmail.com>
2022-02-08 13:45:14 -0500

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,234 @@
 // implement your posts router here
+const express = require('express')
+const Post = require('./posts-model')
+const router = express.Router()
+
+// // - If there's an error in retrieving the _posts_ from the database:
+//   - respond with HTTP status code `500`.
+//   - return the following JSON: `{ message: "The posts information could not be retrieved" }`.
+
+// [GET] /api/posts                                                       
+// √ [1] can get the correct number of posts (40 ms)                    
+// √ [2] can get all the correct posts (26 ms)
+router.get('/', (req, res) => {
+    Post.find()
+    .then(found => {
+        res.json(found)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The posts information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+})
+// - If the _post_ with the specified `id` is not found:
+
+//   - return HTTP status code `404` (Not Found).
+//   - return the following JSON: `{ message: "The post with the specified ID does not exist" }`.
+
+// - If there's an error in retrieving the _post_ from the database:
+//   - respond with HTTP status code `500`.
+//   - return the following JSON: `{ message: "The post information could not be retrieved" }`.
+
+// [GET] /api/posts/:id                                                   
+// √ [3] can get all the correct posts by id (31 ms)                    
+// √ [4] reponds with a 404 if the post is not found (16 ms)
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist",
+            })
+        } else {
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+})
+
+// - If the request body is missing the `title` or `contents` property:
+
+//   - respond with HTTP status code `400` (Bad Request).
+//   - return the following JSON: `{ message: "Please provide title and contents for the post" }`.
+
+// - If the information about the _post_ is valid:
+
+//   - save the new _post_ the the database.
+//   - return HTTP status code `201` (Created).
+//   - return the newly created _post_.
+
+// - If there's an error while saving the _post_:
+//   - respond with HTTP status code `500` (Server Error).
+//   - return the following JSON: `{ message: "There was an error while saving the post to the database" }`.
+
+// [POST] /api/posts                                                      
+// √ [5] responds with a 201 (23 ms)                                    
+// √ [6] responds with a new post (35 ms)                               
+// √ [7] on missing title or contents responds with a 400 (15 ms)
+
+router.post('/', (req, res) => {
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post",
+        })
+    } else {
+        Post.insert({ title, contents })
+        .then(({ id }) => {
+            return Post.findById(id)
+        })
+        .then(post => {
+            res.status(201).json(post)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "There was an error while saving the post to the database",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+    }
+})
+
+// - If the _post_ with the specified `id` is not found:
+
+//   - return HTTP status code `404` (Not Found).
+//   - return the following JSON: `{ message: "The post with the specified ID does not exist" }`.
+
+// - If the request body is missing the `title` or `contents` property:
+
+//   - respond with HTTP status code `400` (Bad Request).
+//   - return the following JSON: `{ message: "Please provide title and contents for the post" }`.
+
+// - If there's an error when updating the _post_:
+
+//   - respond with HTTP status code `500`.
+//   - return the following JSON: `{ message: "The post information could not be modified" }`.
+
+// - If the post is found and the new information is valid:
+
+//   - update the post document in the database using the new information sent in the `request body`.
+//   - return HTTP status code `200` (OK).
+//   - return the newly updated _post_.
+
+// [PUT] /api/posts/:id                                                   
+// √ [8] responds with updated user (15 ms)                             
+// √ [9] saves the updated user to the db (24 ms)                       
+// √ [10] responds with the correct message & status code on bad id (13 ms)                                                                        
+// √ [11] responds with the correct message & status code on validation problem (17 ms)
+
+router.put('/:id', (req, res) => {
+    const { title, contents } = req.body
+    if(!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post",
+        })
+    } else {
+        Post.findById(req.params.id)
+        .then(stuff => {
+            if (!stuff) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist",
+                })
+            } else {
+                return Post.update(req.params.id, req.body)
+            }
+        })
+        .then(data => {
+            if (data) {
+                return Post.findById(req.params.id)
+            }
+        })
+        .then(post => {
+            if (post) {
+                res.json(post)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The post information could not be modified",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+    }
+})
+
+// - If the _post_ with the specified `id` is not found:
+
+//   - return HTTP status code `404` (Not Found).
+//   - return the following JSON: `{ message: "The post with the specified ID does not exist" }`.
+
+// - If there's an error in removing the _post_ from the database:
+
+//   - respond with HTTP status code `500`.
+//   - return the following JSON: `{ message: "The post could not be removed" }`.
+
+// [DELETE] /api/posts/:id
+// √ [12] reponds with a 404 if the post is not found (14 ms)           
+// √ [13] reponds with the complete deleted post (13 ms)                
+// √ [14] removes the deleted post from the database (19 ms)
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist",
+            })
+        } else {
+            await Post.remove(req.params.id)
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post could not be removed",
+            err: err.message,
+            stack: err.stack, 
+        })
+    }
+})
+
+// - If the _post_ with the specified `id` is not found:
+
+//   - return HTTP status code `404` (Not Found).
+//   - return the following JSON: `{ message: "The post with the specified ID does not exist" }`.
+
+// - If there's an error in retrieving the _comments_ from the database:
+
+//   - respond with HTTP status code `500`.
+//   - return the following JSON: `{ message: "The comments information could not be retrieved" }`.
+
+// [GET] /api/posts/:id/comments                                          
+// √ [15] reponds with a 404 if the post is not found (8 ms)            
+// √ [16] can get all the comments associated to the posts with given id (23 ms) 
+
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist",
+            })
+        } else {
+            const messages = await Post.findPostComments(req.params.id)
+            res.json(messages)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The comments information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+})
+
+module.exports = router
